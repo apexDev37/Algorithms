3c941c40eecbeabbebf9d7d789862b964964b537
apexdev <mwangi.em37@gmail.com>
2022-09-04 15:28:28 +0300

    feat: Update main script
    Implemented code for 2 functions: filter_values_by_pairs and picking numbers.

--- easy/pickingNumbers/main.py
+++ easy/pickingNumbers/main.py
@@ -7,7 +7,10 @@
 """
 
 def picking_numbers(array_size: int, values: list[int]) -> int:
-    pass
+    non_duplicate_values = remove_duplicate_values(sorted(values))
+    subarray_pairs = get_subarray_pairs(non_duplicate_values)
+    subarrays = filter_values_by_pair(subarray_pairs, sorted(values))
+    return max(len(subarray) for subarray in subarrays)
 
 def remove_duplicate_values(values: list[int]) -> list[int]:
     values_set = {value for value in values}
@@ -19,11 +22,15 @@ def get_subarray_pairs(values: list[int]) -> list[tuple]:
         if values[v + 1] - values[v] <= 1:
             pairs.append((values[v], values[v + 1]))
     return pairs
-    
+
+def filter_values_by_pair(subarray_pairs: tuple, values: list[int]) -> list[int]:
+    return [list(filter(lambda x: x in subarray, values)) 
+            for subarray in subarray_pairs]
+
+
 def main() -> None:
     array = []
     print(picking_numbers())
-
 
 if __name__ == "__main__":
     main()
