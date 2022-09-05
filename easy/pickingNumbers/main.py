01b9060bb2d489c7acbae5e8816576cc22081998
apexdev <mwangi.em37@gmail.com>
2022-09-05 10:14:20 +0300

    fix: Add new functionality
    Add function: filter_values_by_digit to solve some test cases failing on the HackerRank platform.

--- easy/pickingNumbers/main.py
+++ easy/pickingNumbers/main.py
@@ -6,10 +6,12 @@
     Author: apexDev37
 """
 
-def picking_numbers(array_size: int, values: list[int]) -> int:
+
+def picking_numbers(values: list[int]) -> int:
     non_duplicate_values = remove_duplicate_values(sorted(values))
     subarray_pairs = get_subarray_pairs(non_duplicate_values)
     subarrays = filter_values_by_pair(subarray_pairs, sorted(values))
+    subarrays += filter_values_by_digit(sorted(values))
     return max(len(subarray) for subarray in subarrays)
 
 def remove_duplicate_values(values: list[int]) -> list[int]:
@@ -27,10 +29,13 @@ def filter_values_by_pair(subarray_pairs: tuple, values: list[int]) -> list[int]
     return [list(filter(lambda x: x in subarray, values)) 
             for subarray in subarray_pairs]
 
+def filter_values_by_digit(values: list[int]) -> list[list[int]]:
+    subsets = [list(filter(lambda x: x == g, values)) for g in set(values)]
+    return [subset for subset in subsets if len(subset) > 1]
 
 def main() -> None:
-    array = []
-    print(picking_numbers())
+    numbers = [1, 2, 2, 3, 1, 2]
+    print('Largest subarray lenght: ', picking_numbers(numbers))
 
 if __name__ == "__main__":
     main()
