6bd3e970a2aa2a7e0b5caed7a9d6ecf28bdfed2b
apexdev <mwangi.em37@gmail.com>
2022-09-06 08:56:57 +0300

    refactor: Update variable names
    Refactor variable identifiers in the main script from 'values' to 'numbers' for clarity and module consistency.

--- easy/pickingNumbers/main.py
+++ easy/pickingNumbers/main.py
@@ -3,39 +3,39 @@
     Problem: Picking Numbers
     Level: Easy
     Author: wanbo
-    Author: apexDev37
+    Implementation: apexDev37
 """
 
 
-def picking_numbers(values: list[int]) -> int:
-    non_duplicate_values = remove_duplicate_values(sorted(values))
-    subarray_pairs = get_subarray_pairs(non_duplicate_values)
-    subarrays = filter_values_by_pair(subarray_pairs, sorted(values))
-    subarrays += filter_values_by_digit(sorted(values))
+def picking_numbers(numbers: list[int]) -> int:
+    non_duplicate_numbers = remove_duplicate_numbers(sorted(numbers))
+    subarray_pairs = get_subarray_pairs(non_duplicate_numbers)
+    subarrays = filter_numbers_by_pair(subarray_pairs, sorted(numbers))
+    subarrays += filter_numbers_by_digit(sorted(numbers))
     return max(len(subarray) for subarray in subarrays)
 
-def remove_duplicate_values(values: list[int]) -> list[int]:
-    values_set = {value for value in values}
-    return list(values_set)
+def remove_duplicate_numbers(numbers: list[int]) -> list[int]:
+    numbers_set = {number for number in numbers}
+    return list(numbers_set)
 
-def get_subarray_pairs(values: list[int]) -> list[tuple]:
+def get_subarray_pairs(numbers: list[int]) -> list[tuple]:
     pairs = []
-    for v in range(len(values[:-1])):
-        if values[v + 1] - values[v] <= 1:
-            pairs.append((values[v], values[v + 1]))
+    for n in range(len(numbers[:-1])):
+        if numbers[n + 1] - numbers[n] <= 1:
+            pairs.append((numbers[n], numbers[n + 1]))
     return pairs
 
-def filter_values_by_pair(subarray_pairs: tuple, values: list[int]) -> list[int]:
-    return [list(filter(lambda x: x in subarray, values)) 
+def filter_numbers_by_pair(subarray_pairs: tuple, numbers: list[int]) -> list[int]:
+    return [list(filter(lambda x: x in subarray, numbers)) 
             for subarray in subarray_pairs]
 
-def filter_values_by_digit(values: list[int]) -> list[list[int]]:
-    subsets = [list(filter(lambda x: x == g, values)) for g in set(values)]
+def filter_numbers_by_digit(numbers: list[int]) -> list[list[int]]:
+    subsets = [list(filter(lambda x: x == number, numbers)) for number in set(numbers)]
     return [subset for subset in subsets if len(subset) > 1]
 
 def main() -> None:
     numbers = [1, 2, 2, 3, 1, 2]
-    print('Largest subarray lenght: ', picking_numbers(numbers))
+    print('Largest subarray length: ', picking_numbers(numbers))
 
 if __name__ == "__main__":
     main()
