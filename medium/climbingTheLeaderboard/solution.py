5e46d5f1bd8c31f918848c1d65dd79b9f35f09b6
apexdev <mwangi.em37@gmail.com>
2022-09-08 09:24:37 +0300

    feat: Add new question
    Add template dir and boilerplate code for new HackerRank technical question: Climbing the Leaderboard.

---
+++ medium/climbingTheLeaderboard/solution.py
@@ -0,0 +1,12 @@
+"""
+|| HackerRank
+    Problem: 'problem name'
+    Level: 'problem level'
+    Author: 'problem auther'
+    Implementation: 'your GitHub username'
+
+    Status: PASSED - '# of test cases passed'
+"""
+
+# Copy and paste all your functional code from
+# from the HackerRank platform.
