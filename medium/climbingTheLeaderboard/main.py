d0e57544673d5aba0db7b095e50b2c3192eb4413
apexdev <mwangi.em37@gmail.com>
2022-09-09 16:05:09 +0300

    feat: Add tested functions
    Implement code for 2 tested functions: update_leaderboard and get_player_rank.

--- medium/climbingTheLeaderboard/main.py
+++ medium/climbingTheLeaderboard/main.py
@@ -17,6 +17,15 @@ def update_highscore_rank(score:int, player_rank:list[int]) -> list[int]:
 def is_highscore(score:int, leaderboard:list[int]) -> bool:
     return score > leaderboard[0]
 
+def update_leaderboard(score:int, leaderboard:list[int]) -> list[int]:
+    leaderboard.append(score)
+    non_duplicates = set(leaderboard)
+    return sorted(non_duplicates, reverse=True)
+    
+def get_player_rank(score:int, leaderboard:list[int]) -> int:
+    player_rank = leaderboard.index(score)
+    return player_rank + 1
+
 def main() -> None:
     leaderboard = [100, 90, 90, 80, 75, 60]
     player_scores = [50, 65, 77, 90, 102]
