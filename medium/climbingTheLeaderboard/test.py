ae3ef21a0036ffa1baf56b47827fe1743852a875
apexdev <mwangi.em37@gmail.com>
2022-09-15 07:41:14 +0300

    test: Add tests
    Add TestCases: TestUpdatePlayerRank and TestClimbingTheLeaderboard to capture specified algorithm behavior for steps: 16 and 17.

--- medium/climbingTheLeaderboard/test.py
+++ medium/climbingTheLeaderboard/test.py
@@ -1,10 +1,12 @@
 import unittest
 
 from main import (
+    climbing_leaderboard,
     is_highscore,
     update_highscore_rank,
     update_leaderboard,
-    get_player_rank    
+    get_player_rank,
+    update_player_ranks    
 )
 
 
@@ -16,8 +18,11 @@ Tests for problem: Climbing the Leaderboard
 Script: main.py
 """
 
+
 LEADERBOARD: list[int] = [100, 100, 50, 40, 40, 20, 10]
 PLAYER_SCORES: list[int] = [5, 25, 50, 120]
+
+
 
 
 class TestIsHighscore(unittest.TestCase):
@@ -45,7 +50,7 @@ class TestIsHighscore(unittest.TestCase):
         self.assertEqual(actual, expected)
 
 
-class TestUpdatePlayerRank(unittest.TestCase):
+class TestUpdateHighscoreRank(unittest.TestCase):
 
     def test_should_update_rank_to_one_when_player_achieves_new_highscore(self):
         # Given
@@ -65,7 +70,7 @@ class TestUpdateLeaderboard(unittest.TestCase):
     def test_should_update_and_prepare_leaderboard_with_given_player_score(self):
         """"
         Tests that a given score parameter is updated in the leaderboard list.
-        Prepares leaderboard list to retrieve the player rank from a given score.
+        Prepares leaderboard list to retrieve the player rank from\ a given score.
 
         Asserts:
             - Returns list of integers
@@ -104,4 +109,38 @@ class TestGetPlayerRank(unittest.TestCase):
         self.assertIsInstance(actual, int)
         self.assertGreaterEqual(actual, 1)
         self.assertLessEqual(actual, total_ranks)
+        self.assertEqual(actual, expected)
+
+class TestUpdatePlayerRank(unittest.TestCase):
+
+    def test_should_update_given_player_score_with_corresponding_player_rank(self):
+
+        # Given
+        rank = 2
+        score = 50
+        expected = [5, 25, 2, 120]
+
+        # When
+        actual = update_player_ranks(rank, score, PLAYER_SCORES)
+        
+        # Then
+        self.assertIsInstance(actual, list)
+        self.assertIsInstance(actual[0], int)
+        self.assertIn(rank, actual)
+        self.assertNotIn(score, actual)
+        self.assertEqual(actual, expected)
+
+class TestClimbingTheLeaderboard(unittest.TestCase):
+
+    def test_should_return_player_ranks_from_leaderboard_for_all_player_scores(self):
+
+        # Given
+        expected = [6, 4, 2, 1]
+
+        # When
+        actual = climbing_leaderboard(LEADERBOARD, PLAYER_SCORES)
+        
+        # Then
+        self.assertIsInstance(actual, list)
+        self.assertIsInstance(actual[0], int)
         self.assertEqual(actual, expected)
