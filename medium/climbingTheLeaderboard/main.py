77a5ac6393a452d599cac3b6fdbc25a20ae28c5d
apexdev <mwangi.em37@gmail.com>
2022-09-17 11:11:33 +0300

    refactor: Extract function
    Extract function: get_player_ranks to increase abstraction and code readability.

--- medium/climbingTheLeaderboard/main.py
+++ medium/climbingTheLeaderboard/main.py
@@ -9,16 +9,23 @@
 import bisect
 
 def climbing_leaderboard(leaderboard:list[int], player_scores:list[int]) -> list[int]:
-    player_ranks = []
     leaderboard = list(dict.fromkeys(leaderboard))[::-1]
+    player_ranks = get_player_ranks(leaderboard, player_scores)
+    return player_ranks[::-1]
+
+
+def get_player_ranks(leaderboard:list[int], player_scores:list[int]) -> list[int]:
+    player_ranks = []
     for score in reversed(player_scores):
         rank = get_player_rank(score, leaderboard)
         player_ranks.append(rank)
-    return player_ranks[::-1]
-    
+    return player_ranks
+
+
 def get_player_rank(score:int, leaderboard:list[int]) -> int:
     player_rank = bisect.bisect_right(leaderboard, score)
     return (len(leaderboard) - player_rank) + 1
+
 
 def main() -> None:
     leaderboard = [100, 100, 50, 40, 40, 20, 10]
