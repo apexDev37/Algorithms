b9115ffc1a3f77c9fd6587b0a22fbe6399115766
apexdev <mwangi.em37@gmail.com>
2022-09-17 11:45:32 +0300

    feat: Add code solution
    Add code solution that was used to pass all test cases run on the HackerRank platform.

--- medium/climbingTheLeaderboard/solution.py
+++ medium/climbingTheLeaderboard/solution.py
@@ -1,12 +1,29 @@
 """
 || HackerRank
-    Problem: 'problem name'
-    Level: 'problem level'
-    Author: 'problem auther'
-    Implementation: 'your GitHub username'
+    Problem: Climbing the Leaderboard 
+    Level: medium
+    Author: Shafaet
+    Implementation: apexDev37
 
-    Status: PASSED - '# of test cases passed'
+    Status: PASSED - 11 test cases
 """
 
-# Copy and paste all your functional code from
-# from the HackerRank platform.
+import bisect
+
+def climbingLeaderboard(leaderboard, player_scores):
+    leaderboard = list(dict.fromkeys(leaderboard))[::-1]
+    player_ranks = get_player_ranks(leaderboard, player_scores)
+    return player_ranks[::-1]
+
+
+def get_player_ranks(leaderboard:list[int], player_scores:list[int]) -> list[int]:
+    player_ranks = []
+    for score in reversed(player_scores):
+        rank = get_player_rank(score, leaderboard)
+        player_ranks.append(rank)
+    return player_ranks
+
+
+def get_player_rank(score:int, leaderboard:list[int]) -> int:
+    player_rank = bisect.bisect_right(leaderboard, score)
+    return (len(leaderboard) - player_rank) + 1
