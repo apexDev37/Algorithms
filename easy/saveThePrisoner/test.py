74a3143677db3caa01f32b7436971511f7a90760
apexdev <mwangi.em37@gmail.com>
2022-09-19 10:03:41 +0300

    test: Add test
    Add single test case: TestSaveThePrisoner to unit test function: save_the_prisoner.

--- easy/saveThePrisoner/test.py
+++ easy/saveThePrisoner/test.py
@@ -1,23 +1,39 @@
 import unittest
 
-# from main import (
-#     # function_example_one,
-#     # function_example_two,
-#     # function_example_three
-# )
+from main import (
+    save_the_prisoner
+)
 
 
 # ---------------------------- Tests ---------------------------- #
 
 
 """
-Tests for problem: 'Problem name'
+Tests for problem: Save The Prisoner
 Script: main.py
 """
 
 
-# See example sample tests in
-# .
-# |-- easy
-#   |-- question
-#       |-- test.py
+PRISONERS: int = 7
+SWEETS: int = 19
+CHAIR_POSITION: int = 2
+
+
+class TestSaveThePrisoner(unittest.TestCase):
+    
+    def test_should_return_chair_position_to_receive_the_last_sweet(self) -> None:
+        # Given
+        expected = 6
+
+        # When        
+        actual = save_the_prisoner(CHAIR_POSITION, PRISONERS, SWEETS)
+
+        # Then
+        self.assertIsInstance(actual, type(expected))        
+        self.assertGreaterEqual(actual, 1)        
+        self.assertLessEqual(actual, PRISONERS)        
+        self.assertEqual(actual, expected)        
+
+
+if __name__ == '__main__':
+    unittest.main()
