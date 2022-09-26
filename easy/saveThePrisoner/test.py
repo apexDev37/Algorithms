289bc5cdf5474767901dca6ebd57c5c87126545a
apexdev <mwangi.em37@gmail.com>
2022-09-26 08:58:40 +0300

    test: Add test
    Add test for function: get_remainder_sweets to verify implementation of algorithm.

--- easy/saveThePrisoner/test.py
+++ easy/saveThePrisoner/test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from main import (
+    get_remainder_sweets,
     save_the_prisoner
 )
 
@@ -20,7 +21,26 @@ CHAIR_POSITION: int = 2
 
 
 class TestSaveThePrisoner(unittest.TestCase):
-    
+
+    def test_should_compute_modulus_of_sweets_and_prisoners(self) -> None:
+        """
+        || Test for function: get_remainder_sweets
+            Checks if the number of sweets is greater
+            than the number of prisoners and computes
+            the modulus if greater.
+        """
+
+        # Given
+        expected = 5
+
+        # When
+        actual = get_remainder_sweets(PRISONERS, SWEETS)
+        
+        # Then
+        self.assertIsInstance(actual, int)
+        self.assertEqual(actual, expected)
+
+
     def test_should_return_chair_position_to_receive_the_last_sweet(self) -> None:
         # Given
         expected = 6
