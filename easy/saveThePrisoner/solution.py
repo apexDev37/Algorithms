449907fb86e03d6e481d01dd13f1b27392e36f23
apexdev <mwangi.em37@gmail.com>
2022-09-26 09:02:08 +0300

    feat: Add code solution
    Add code solution that was used to pass all test cases run on the HackerRank platform.

--- easy/saveThePrisoner/solution.py
+++ easy/saveThePrisoner/solution.py
@@ -1,12 +1,30 @@
 """
 || HackerRank
-    Problem: 'problem name'
-    Level: 'problem level'
-    Author: 'problem author'
-    Implementation: 'your GitHub username'
+    Problem: Save The Prisoner
+    Level: Easy
+    Author: sd5869 
+    Implementation: apexDev37
 
-    Status: PASSED - '# of test cases passed'
+    Status: PASSED - 11 test cases passed
 """
 
 # Copy and paste all your functional code from
-# from the HackerRank platform.
+# the HackerRank platform.
+
+def saveThePrisoner(chair_position: int, prisoners: int, sweets: int) -> int:
+    sweets = get_remainder_sweets(prisoners, sweets)
+    new_position = chair_position + sweets
+    prisoner = get_chair_position(new_position, prisoners) 
+    return prisoner
+
+
+def get_remainder_sweets(prisoners: int, sweets: int) -> int:
+    return (sweets % prisoners if sweets > prisoners
+        else sweets)
+
+
+def get_chair_position(new_position: int, prisoners: int) -> int:
+    if new_position > prisoners:
+        new_position -= prisoners
+    return (prisoners if new_position == 1 
+        else new_position - 1)
