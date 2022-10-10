dc6196ba60cb768e5fad367034b5315922507b02
apexdev <mwangi.em37@gmail.com>
2022-10-10 09:14:51 +0300

    test: Add TestCase
    Add TestCase for Append and Delete. Add initial test class and function for the function: append_new_string()

--- easy/appendAndDelete/test.py
+++ easy/appendAndDelete/test.py
@@ -1,10 +1,8 @@
 import unittest
 
-# from main import (
-#     # function_example_one,
-#     # function_example_two,
-#     # function_example_three
-# )
+from main import (
+    append_by_empty_string
+)
 
 
 # ---------------------------- Tests ---------------------------- #
@@ -16,8 +14,35 @@ Script: main.py
 """
 
 
-# See example sample tests in
-# .
-# |-- easy
-#   |-- question
-#       |-- test.py
+INITIAL_STRING: str = 'hackerhappy'
+DESIRED_STRING: str = 'hackerrank'
+MOVES: int = 9
+
+
+class TestAppendAndDelete(unittest.TestCase):
+    
+    def test_should_compute_if_can_create_empty_string_and_append_desired_string(self) -> None:
+        """ 
+            Test to determine if the initial string can be
+            converted to an empty string (delete) and 
+            replaced by desired string (append) all in
+            the given amount of moves. 
+        """
+        
+        # Given
+        # --- Reference constant values defined above
+        expected = 'No'
+        
+        # When
+        actual = append_by_empty_string(INITIAL_STRING, DESIRED_STRING, MOVES)
+        
+        # Then
+        self.assertIsInstance(actual, type(expected))
+        self.assertEquals(actual, 'Yes' or 'No')
+        self.assertEqual(actual, expected)
+        
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
