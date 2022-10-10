e94bc5c426247d4b60657357f7560711ee95c4ec
apexdev <mwangi.em37@gmail.com>
2022-10-10 08:36:51 +0300

    feat: Initial commit
    Initial commit for the question: Append And Delete. Create all necessary files and boilerplate from the python template to start programming.

---
+++ easy/appendAndDelete/main.py
@@ -0,0 +1,23 @@
+"""
+|| HackerRank
+    Problem: 'problem name' 
+    Level: 'problem level'
+    Author: 'problem author'
+    Implementation: 'your GitHub username'
+"""
+
+# This is main entry point function to complete
+# on the HackerRank platform. 
+# Rename as per specified
+
+def function_name():
+    pass
+
+
+def main() -> None:
+    # Define your actual argument data here
+    print('Meaningful message output: ', function_name())
+
+
+if __name__ == "__main__":
+    main()
