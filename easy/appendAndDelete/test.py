f116af2db5baf4ff4d452605d7e18b1950568900
apexdev <mwangi.em37@gmail.com>
2022-10-24 17:04:34 +0300

    test: Add tests
    Add tests to faciltate refactoring code design in the main script. Tests for functions : append_by_matching_chars() and append_by_substrings().

--- easy/appendAndDelete/test.py
+++ easy/appendAndDelete/test.py
@@ -5,7 +5,9 @@ from main import (
     append_by_empty_string,
     get_substring_index,
     get_substring_lengths,
-    matching_chars
+    matching_chars,
+    append_by_matching_chars,
+    append_by_substrings
 )
 
 
@@ -109,13 +111,36 @@ class TestAppendAndDelete(unittest.TestCase):
         self.assertIsInstance(actual, type(expected))
         self.assertEqual(actual, expected)
 
-    def test_should_check_if_moves_exist_for_strings_with_matching_chars(self) -> None:
+    def test_should_return_yes_if_enough_moves_to_append_and_delete_matching_char_strings(self) -> None:
         """
-        Test if the initial and desired strings that have
-        the matching characters without considering
-        string length.
+        Test if able to append and delete the initial string
+        to the desired string if both strings have matching 
+        characters without considering string length.
         """
         
+        # Given
+        initial_string = 'aaaaa'
+        desired_string = 'aaaaaaa'
+        moves = 1
+        expected = 'No'
+        
+        # When
+        actual = append_by_matching_chars(initial_string, desired_string, moves)
+        
+        # Then
+        self.assertIsNotNone(actual)
+        self.assertIsInstance(actual, type(expected))
+        self.assertEqual(actual, expected)
+
+
+    def test_should_return_true_if_strings_have_same_chars_sequence(self) -> None:
+        """
+        Test should return a boolean value based on
+        if the initial and desired string both have a 
+        string sequence of a single matching char
+        regardless of the length of the two strings.
+        """
+
         # Given
         initial_string = 'aaaaa'
         desired_string = 'aaaaaaa'
@@ -128,6 +153,26 @@ class TestAppendAndDelete(unittest.TestCase):
         self.assertIsNotNone(actual)
         self.assertIsInstance(actual, type(expected))
         self.assertEqual(actual, expected)
+        
+
+    def test_should_return_yes_if_can_replace_substrings_to_attain_desired_strings(self) -> None:
+        """
+        Test should determine if the number of available moves
+        equals the sum of the lengths of the differing substrings
+        in the initial and desired string.
+        """
+        
+        # Given
+        expected = 'Yes'
+        
+        # When
+        actual = append_by_substrings(INITIAL_STRING, DESIRED_STRING, MOVES)
+        
+        # Then
+        self.assertIsNotNone(actual)
+        self.assertIsInstance(actual, type(expected))
+        self.assertEqual(actual, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
