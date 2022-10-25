dbdfd3ed8fa48e975847203908d16928266dfccf
apexdev <mwangi.em37@gmail.com>
2022-10-25 17:17:39 +0300

    style: Format scripts
    Format scripts with autopep8 package to adhere to the Python PEP8 style guide.

--- easy/appendAndDelete/solution.py
+++ easy/appendAndDelete/solution.py
@@ -1,6 +1,6 @@
 """
 || HackerRank
-    Problem: Append and Delete 
+    Problem: Append and Delete
     Level: Easy
     Author: zemen
     Implementation: apexDev37
@@ -8,7 +8,8 @@
     Status: PASSED - '# of test cases passed'
 """
 
-def appendAndDelete(initial:str, desired:str, moves:int) -> str:
+
+def appendAndDelete(initial: str, desired: str, moves: int) -> str:
     response = append_by_empty_string(initial, desired, moves)
     if response == 'No':
         if matching_chars(initial, desired):
@@ -18,47 +19,50 @@ def appendAndDelete(initial:str, desired:str, moves:int) -> str:
     return response
 
 
-def append_by_empty_string(initial:str, desired:str, moves:int) -> str:
+def append_by_empty_string(initial: str, desired: str, moves: int) -> str:
     delete_moves = len(initial)
     append_moves = len(desired)
     total_moves = delete_moves + append_moves
     return 'Yes' if (total_moves <= moves) else 'No'
 
- 
-def matching_chars(initial:str, desired:str) -> bool:
-    return (   
+
+def matching_chars(initial: str, desired: str) -> bool:
+    return (
         initial == len(initial) * initial[0]
         and desired == len(desired) * desired[0]
         and initial[0] == desired[0]
     )
 
 
-def append_by_matching_chars(initial:str, desired:str, moves:int) -> str:
+def append_by_matching_chars(initial: str, desired: str, moves: int) -> str:
     response = 'No'
     difference = abs(len(desired) - len(initial))
-    if (difference % 2) == (moves % 2): response = 'Yes'
+    if (difference % 2) == (moves % 2):
+        response = 'Yes'
     return response
 
 
-def append_by_substrings(initial:str, desired:str, moves:int) -> str:
-    if (initial == desired): return 'Yes'
+def append_by_substrings(initial: str, desired: str, moves: int) -> str:
+    if (initial == desired):
+        return 'Yes'
     index = get_substring_index(initial, desired)
     substring_lengths = get_substring_lengths(index, initial, desired)
     response = 'Yes' if (substring_lengths == moves) else 'No'
-    return response    
+    return response
 
 
-def get_substring_index(initial:str, desired:str) -> int:
+def get_substring_index(initial: str, desired: str) -> int:
     substring_index = 0
-    for tuple in list(enumerate(zip(initial, desired))):    # Tuple format: [0, ('h', 'h')]
+    for tuple in list(enumerate(zip(initial, desired)
+                                )):    # Tuple format: [0, ('h', 'h')]
         chars = tuple[1]
         if chars[0] != chars[1]:
-            substring_index = tuple[0] 
+            substring_index = tuple[0]
             break
     return substring_index
 
 
-def get_substring_lengths(index:int, initial:str, desired:str) -> int:
+def get_substring_lengths(index: int, initial: str, desired: str) -> int:
     initial_substring = initial[index:]
     desired_substring = desired[index:]
     return len(initial_substring) + len(desired_substring)
--- easy/appendAndDelete/test.py
+++ easy/appendAndDelete/test.py
@@ -26,117 +26,119 @@ MOVES: int = 9
 
 
 class TestAppendAndDelete(unittest.TestCase):
-    
-    def test_should_compute_if_can_create_empty_string_and_append_desired_string(self) -> None:
-        """ 
-            Test to determine if the initial string can be
-            converted to an empty string (delete) and 
-            replaced by desired string (append) all in
-            the given amount of moves. 
+
+    def test_should_compute_if_can_create_empty_string_and_append_desired_string(
+            self) -> None:
         """
-        
+            Test to determine if the initial string can be
+            converted to an empty string (delete) and
+            replaced by desired string (append) all in
+            the given amount of moves.
+        """
+
         # Given
         # --- Reference constant values defined above
         expected = 'No'
-        
+
         # When
         actual = append_by_empty_string(INITIAL_STRING, DESIRED_STRING, MOVES)
-        
+
         # Then
         self.assertIsInstance(actual, type(expected))
         self.assertIn(actual, ['Yes', 'No'])
         self.assertEqual(actual, expected)
-        
-        
+
     def test_should_get_the_index_of_first_occurring_char_change(self) -> None:
         """
         Test behavior to retrieve the index of the first
         character which is not equal in the initial and
-        desired strings. 
+        desired strings.
             Initial: hackerhappy
             Desired: hackerrank
             Index: 6 ('h' != 'r')
         """
-        
+
         # Given
         # --- Reference constant values defined above
         expected = 6
 
-        # When 
+        # When
         actual = get_substring_index(INITIAL_STRING, DESIRED_STRING)
-        
-        # Then 
+
+        # Then
         self.assertIsNotNone(actual)
         self.assertIsInstance(actual, type(expected))
         self.assertLessEqual(actual, len(INITIAL_STRING))
         self.assertEqual(actual, expected)
-    
-    
-    def test_should_compute_and_return_substring_lengths_to_append_and_delete(self) -> None:
+
+    def test_should_compute_and_return_substring_lengths_to_append_and_delete(
+            self) -> None:
         """
         Test behavior to get the lengths of the substrings
         in the initial and desired string. Use the return
         value to determine if there are enough moves to
         perform the append and delete.
         """
-        
+
         # Given
         index = 6
         expected = 9
-        
+
         # When
         actual = get_substring_lengths(index, INITIAL_STRING, DESIRED_STRING)
-        
-        # Then 
-        self.assertIsNotNone(actual)
-        self.assertIsInstance(actual, type(expected))
-        self.assertEqual(actual, expected)
-        
-    
-    def test_should_respond_yes_or_no_based_on_append_and_delete_moves(self) -> None:
-        """
-        Test the append and delete functionality to create 
-        the desired string from the initial string based 
-        on the given moves to append and delete
-        """
-        
-        # Given
-        expected = 'Yes'
-        
-        # When
-        actual = append_and_delete(INITIAL_STRING, DESIRED_STRING, MOVES)
-        
+
         # Then
         self.assertIsNotNone(actual)
         self.assertIsInstance(actual, type(expected))
         self.assertEqual(actual, expected)
 
-    def test_should_return_yes_if_enough_moves_to_append_and_delete_matching_char_strings(self) -> None:
+    def test_should_respond_yes_or_no_based_on_append_and_delete_moves(
+            self) -> None:
+        """
+        Test the append and delete functionality to create
+        the desired string from the initial string based
+        on the given moves to append and delete
+        """
+
+        # Given
+        expected = 'Yes'
+
+        # When
+        actual = append_and_delete(INITIAL_STRING, DESIRED_STRING, MOVES)
+
+        # Then
+        self.assertIsNotNone(actual)
+        self.assertIsInstance(actual, type(expected))
+        self.assertEqual(actual, expected)
+
+    def test_should_return_yes_if_enough_moves_to_append_and_delete_matching_char_strings(
+            self) -> None:
         """
         Test if able to append and delete the initial string
-        to the desired string if both strings have matching 
+        to the desired string if both strings have matching
         characters without considering string length.
         """
-        
+
         # Given
         initial_string = 'aaaaa'
         desired_string = 'aaaaaaa'
         moves = 1
         expected = 'No'
-        
+
         # When
-        actual = append_by_matching_chars(initial_string, desired_string, moves)
-        
+        actual = append_by_matching_chars(
+            initial_string, desired_string, moves)
+
         # Then
         self.assertIsNotNone(actual)
         self.assertIsInstance(actual, type(expected))
         self.assertEqual(actual, expected)
 
-
-    def test_should_return_true_if_strings_have_same_chars_sequence(self) -> None:
+    def test_should_return_true_if_strings_have_same_chars_sequence(
+            self) -> None:
         """
         Test should return a boolean value based on
-        if the initial and desired string both have a 
+        if the initial and desired string both have a
         string sequence of a single matching char
         regardless of the length of the two strings.
         """
@@ -145,29 +147,29 @@ class TestAppendAndDelete(unittest.TestCase):
         initial_string = 'aaaaa'
         desired_string = 'aaaaaaa'
         expected = True
-        
+
         # When
         actual = matching_chars(initial_string, desired_string)
-        
+
         # Then
         self.assertIsNotNone(actual)
         self.assertIsInstance(actual, type(expected))
         self.assertEqual(actual, expected)
-        
 
-    def test_should_return_yes_if_can_replace_substrings_to_attain_desired_strings(self) -> None:
+    def test_should_return_yes_if_can_replace_substrings_to_attain_desired_strings(
+            self) -> None:
         """
         Test should determine if the number of available moves
         equals the sum of the lengths of the differing substrings
         in the initial and desired string.
         """
-        
+
         # Given
         expected = 'Yes'
-        
+
         # When
         actual = append_by_substrings(INITIAL_STRING, DESIRED_STRING, MOVES)
-        
+
         # Then
         self.assertIsNotNone(actual)
         self.assertIsInstance(actual, type(expected))
