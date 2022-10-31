df44a63b221c266e24557bf739f3407d024769b6
apexdev <mwangi.em37@gmail.com>
2022-10-31 08:00:24 +0300

    feat: Initial commit
    Initial commit for the question: Sherlock and Squares. Create all necessary files and boilerplate from the python template to start programming.

---
+++ easy/sherlockAndSquares/test.py
@@ -0,0 +1,23 @@
+import unittest
+
+# from main import (
+#     # function_example_one,
+#     # function_example_two,
+#     # function_example_three
+# )
+
+
+# ---------------------------- Tests ---------------------------- #
+
+
+"""
+Tests for problem: 'Problem name'
+Script: main.py
+"""
+
+
+# See example sample tests in
+# .
+# |-- easy
+#   |-- question
+#       |-- test.py
