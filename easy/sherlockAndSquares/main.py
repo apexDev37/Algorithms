921e886e1972c4cecbc25e951afaa9d04c86a898
apexdev <mwangi.em37@gmail.com>
2022-11-01 11:24:24 +0300

    style: Format scripts
    Format scripts with autopep8 package to adhere to the Python PEP8 style guide.

--- easy/sherlockAndSquares/main.py
+++ easy/sherlockAndSquares/main.py
@@ -1,6 +1,6 @@
 """
 || HackerRank
-    Problem: Sherlock and Squares 
+    Problem: Sherlock and Squares
     Level: easy
     Author: darkshadows
     Implementation: apexDev37
@@ -9,27 +9,31 @@
 import math
 
 
-def sherlock_and_squares(lower_bound:int, upper_bound:int) -> int:
+def sherlock_and_squares(lower_bound: int, upper_bound: int) -> int:
     perfect_square = get_first_perfect_square(lower_bound, upper_bound)
-    perfect_squares = get_perfect_squares(perfect_square, lower_bound, upper_bound)
+    perfect_squares = get_perfect_squares(
+        perfect_square, lower_bound, upper_bound)
     return perfect_squares
 
 
-def get_first_perfect_square(lower_bound:int, upper_bound:int) -> int: 
+def get_first_perfect_square(lower_bound: int, upper_bound: int) -> int:
     first_perfect_square = 0
     for value in range(lower_bound, upper_bound + 1):
         if is_perfect_square(value):
             first_perfect_square = value
             break
-    return first_perfect_square        
+    return first_perfect_square
 
 
-def is_perfect_square(whole_number:int) -> bool:
+def is_perfect_square(whole_number: int) -> bool:
     square_root = math.isqrt(whole_number)
     return whole_number == math.pow(square_root, 2)
-    
 
-def get_perfect_squares(perfect_square:int, lower_bound:int, upper_bound:int) -> int:
+
+def get_perfect_squares(
+        perfect_square: int,
+        lower_bound: int,
+        upper_bound: int) -> int:
     number_of_perfect_squares = 0
     while perfect_square <= upper_bound:
         if is_within_range(perfect_square, lower_bound, upper_bound):
@@ -38,16 +42,19 @@ def get_perfect_squares(perfect_square:int, lower_bound:int, upper_bound:int) ->
     return number_of_perfect_squares
 
 
-def is_within_range(perfect_square:int, lower_bound:int, upper_bound:int) -> bool:
+def is_within_range(
+        perfect_square: int,
+        lower_bound: int,
+        upper_bound: int) -> bool:
     return (lower_bound <= perfect_square <= upper_bound)
 
 
-def next_perfect_square(current_perfect_square:int) -> int:
+def next_perfect_square(current_perfect_square: int) -> int:
     square_root = math.isqrt(current_perfect_square)
     square_root += 1
-    return int(math.pow(square_root, 2))        
+    return int(math.pow(square_root, 2))
 
-    
+
 def main() -> None:
     # lbound = 1
     # ubound = 9
