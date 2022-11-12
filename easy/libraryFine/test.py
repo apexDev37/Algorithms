2768c19f4448cee3455d5e4cbbf81fa1f0070f7b
apexdev <mwangi.em37@gmail.com>
2022-11-12 08:24:12 +0300

    test: Add tests
    Add 2 tests in order to test implementation logic and program behaviour to check if a book is returned on or before the due date and compute the corresponding fine charged.

--- easy/libraryFine/test.py
+++ easy/libraryFine/test.py
@@ -1,10 +1,10 @@
 import unittest
+from datetime import date
 
-# from main import (
-#     # function_example_one,
-#     # function_example_two,
-#     # function_example_three
-# )
+from main import (
+  is_returned_by_due_date,
+  get_fine
+)
 
 
 # ---------------------------- Tests ---------------------------- #
@@ -16,8 +16,41 @@ Script: main.py
 """
 
 
-# See example sample tests in
-# .
-# |-- easy
-#   |-- question
-#       |-- test.py
+DUE_DATE: date = date(2020, 2, 1)
+RETURN_DATE: date = date(2020, 7, 28)
+
+
+class LibraryFine(unittest.TestCase):
+  
+  def test_should_check_if_book_is_returned_on_or_before_due_date(self) -> None:
+  
+    # Given
+    before_due_date: date = date(2020, 2, 1)
+    expected = True 
+    
+    # When
+    actual = is_returned_by_due_date(before_due_date, DUE_DATE)
+    
+    # Then
+    self.assertIsNotNone(actual)
+    self.assertIsInstance(actual, type(expected))
+    self.assertEqual(actual, expected)
+  
+  def test_should_charge_zero_fine_for_book_return_on_or_before_due_date(self) -> None:
+    
+    # Given
+    before_due_date: date = date(2020, 1, 1)
+    expected = 0
+    
+    # When
+    actual = get_fine(before_due_date, DUE_DATE) 
+    
+    # Then
+    self.assertIsNotNone(actual)
+    self.assertIsInstance(actual, type(expected))
+    self.assertEqual(actual, expected)
+
+
+
+if __name__ == '__main__':
+  unittest.main()
