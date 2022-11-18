735580889dc53788bfec73f1f6c66a360bed5fc0
apexdev <mwangi.em37@gmail.com>
2022-11-18 12:22:52 +0300

    chore: Add solution
    Add solution code that passed all tests cases on the HackerRank platform for question: Cut The Sticks. Number of test cases passed: (10/10).

--- easy/cutTheSticks/solution.py
+++ easy/cutTheSticks/solution.py
@@ -5,8 +5,29 @@
     Author: shashank21j
     Implementation: apexDev37
 
-    Status: PASSED - '# of test cases passed'
+    Status: PASSED - '10/10 test cases passed'
 """
 
-# Copy and paste all your functional code from
-# from the HackerRank platform.
+def cutTheSticks(sticks):
+  unique_sticks = sort_and_remove_duplicates(sticks)
+  sticks_cut = get_number_of_sticks_cut(sticks, unique_sticks)
+  return sticks_cut
+
+
+def get_number_of_sticks_cut(sticks: list[int], unique_sticks: list[int]) -> list[int]:
+  number_of_sticks_cut = [len(sticks)]
+  for stick in unique_sticks:
+    sticks = filter_min_value(sticks, stick)
+    number_of_sticks_cut.append(len(sticks))
+  return number_of_sticks_cut[:-1]
+   
+
+def sort_and_remove_duplicates(sticks: list[int]) -> list[int]:
+  non_duplicate_sticks = set(sticks)
+  sorted_sticks = sorted(non_duplicate_sticks)
+  return list(sorted_sticks)
+
+
+def filter_min_value(sticks: list[int], minimum_value: int) -> list[int]:
+  filtered_sticks = list(filter(lambda x: x != minimum_value, sticks))
+  return filtered_sticks
