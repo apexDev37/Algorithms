75ef76cc2ee0e94bf1d1a63767dbd1f384756f5f
apexDev37 <mwangi.em37@gmail.com>
2023-01-09 18:21:17 +0300

    feat: Initial commit
    Add classes Node and LinkedList to provide the necessary building blocks to implement singly linked lists and their their basic operations.

---
+++ algorithms/java/singlyLinkedList/LinkedList.java
@@ -0,0 +1,7 @@
+package com.apexdev.algorithms.linkedlist;
+
+public class LinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
+}
---
+++ algorithms/java/singlyLinkedList/Node.java
@@ -0,0 +1,6 @@
+package com.apexdev.algorithms.linkedlist;
+
+public class Node {
+    public int value;
+    public Node next;
+}
