8251b6028a9c634dec5e835c76f50b051c06e1ff
apexDev37 <mwangi.em37@gmail.com>
2023-01-28 12:50:17 +0300

    feat: Initial commit
    Create directory and necessary base classes to implement Doubly Linked List data structure.

---
+++ algorithms/java/doublyLinkedList/base/LinkedList.java
@@ -0,0 +1,38 @@
+package com.apexdev.algorithms.java.doublyLinkedList.base;
+
+
+public class LinkedList {
+  protected Node head;
+  protected Node tail;
+  protected int size;
+
+  protected boolean emptyLinkedList() {
+    return this.head == null;
+  }
+
+  protected Node getNodeAt(int location) {
+    try {
+      return getNode(location);
+    } catch (NullPointerException exception) {
+      throw new IndexOutOfBoundsException("Given node location: " + location + ", does not exist.");
+    }
+  }
+
+  private Node getNode(int location) {
+    Node currentNode = this.head;
+    for (int n = 0; n < location - 1; n++)
+      currentNode = currentNode.next;
+    return currentNode;
+  }
+
+  protected int getNodeLocation(int target) {
+    Node currentNode = this.head;
+    for (int location = 0; location < this.size; location++) {
+      if (currentNode.value == target)
+        return location;
+      currentNode = currentNode.next;
+    }
+    return -1;
+  }
+
+}
---
+++ algorithms/java/doublyLinkedList/base/Node.java
@@ -0,0 +1,7 @@
+package com.apexdev.algorithms.java.doublyLinkedList.base;
+
+public class Node {
+  public int value;
+  public Node next;
+  public Node prev;
+}
