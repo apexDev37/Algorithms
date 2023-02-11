7fa65408785d01de463583f6d3cd03ceccad4ee0
apexDev37 <mwangi.em37@gmail.com>
2023-02-11 15:56:45 +0300

    feat: Add NthToLast implementation
    Add class NthToLast to define logic to output the node nth from the last node in a linked list. Expose public method: execute to provide implementation.

---
+++ interviewQuestions/linkedList/returnNthToLast/NthToLast.java
@@ -0,0 +1,39 @@
+package com.apexdev.interviewQuestions.linkedList.returnNthToLast;
+
+import com.apexdev.algorithms.java.singlyLinkedList.SingleLinkedList;
+import com.apexdev.algorithms.java.singlyLinkedList.base.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NthToLast {
+  private static final Map<Integer, Node> locationNodeMap = new HashMap<>();
+  private static Node nthNode;
+
+  public static void execute(int nth, SingleLinkedList linkedList) {
+    mapLocationToNode(linkedList);
+    findNthNode(nth);
+    displayDetails(nth, linkedList);
+  }
+
+  private static void mapLocationToNode(SingleLinkedList linkedList) {
+    int location = 0;
+    Node currentNode = linkedList.head;
+    while (currentNode != null) {
+      locationNodeMap.put(location, currentNode);
+      currentNode = currentNode.next;
+      location += 1;
+    }
+  }
+
+  private static void findNthNode(int nth) {
+    int lastNodeLocation = locationNodeMap.size() - 1;
+    int nthLocation = (lastNodeLocation - nth) + 1;
+    nthNode = locationNodeMap.get(nthLocation);
+  }
+
+  private static void displayDetails(int nth, SingleLinkedList linkedList) {
+    linkedList.displayNodes();
+    System.out.printf("Nth Node -> Location: %d, Value: [%d]%n", nth, nthNode.value);
+  }
+}
