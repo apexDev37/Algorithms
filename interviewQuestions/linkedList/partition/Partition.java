6dd932e2e74acfa54a04d700bd3a70e70747e5fd
apexDev37 <mwangi.em37@gmail.com>
2023-02-13 14:30:25 +0300

    refactor: Modularize logic
    Modularize the execute function by extracting methods to encapsulate logical steps and operations to increase code readability and flexibility for future modification.

--- interviewQuestions/linkedList/partition/Partition.java
+++ interviewQuestions/linkedList/partition/Partition.java
@@ -8,24 +8,36 @@ public final class Partition {
   public static void execute(SingleLinkedList linkedList, int target) {
     Node currentNode = linkedList.head;
     linkedList.tail = linkedList.head;
+    partition(linkedList, target, currentNode);
+    outputResults(linkedList, target);
+  }
 
+  private static void partition(SingleLinkedList linkedList, int target, Node currentNode) {
     for (int location = 0; location < linkedList.size; location++) {
       Node nextNode = currentNode.next;
-
-      if (currentNode.value < target) {
-        // Insert current node before head node
-        currentNode.next = linkedList.head;
-        linkedList.head = currentNode;
-      } else {
-        // Insert current node after tail node
-        linkedList.tail.next = currentNode;
-        linkedList.tail = currentNode;
-      }
-
+      insertBasedOnTarget(linkedList, target, currentNode);
       currentNode = nextNode;
     }
-
     linkedList.tail.next = null;
+  }
+
+  private static void insertBasedOnTarget(SingleLinkedList linkedList, int target, Node currentNode) {
+    if (currentNode.value < target)
+      insertBeforeHead(linkedList, currentNode);
+    else insertAfterTail(linkedList, currentNode);
+  }
+
+  private static void insertBeforeHead(SingleLinkedList linkedList, Node currentNode) {
+    currentNode.next = linkedList.head;
+    linkedList.head = currentNode;
+  }
+
+  private static void insertAfterTail(SingleLinkedList linkedList, Node currentNode) {
+    linkedList.tail.next = currentNode;
+    linkedList.tail = currentNode;
+  }
+
+  private static void outputResults(SingleLinkedList linkedList, int target) {
     System.out.printf("Partitioned by target (%d): %n", target);
     linkedList.displayNodes();
   }
