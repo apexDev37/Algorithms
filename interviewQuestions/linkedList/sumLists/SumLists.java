409101693db40b6a1b12bb0e942c5a9d4901805d
apexDev37 <mwangi.em37@gmail.com>
2023-02-25 13:42:32 +0300

    refactor: Clean implementation
    Clean implementation through the following:
    Rename functions to enhance clarity.
    Extract methods to increase code modularity.
    Remove unnecessary comments due to using expressive names.

--- interviewQuestions/linkedList/sumLists/SumLists.java
+++ interviewQuestions/linkedList/sumLists/SumLists.java
@@ -7,42 +7,38 @@ public final class SumLists {
 
   public static void execute(SingleLinkedList firstList,
                              SingleLinkedList secondList) {
-
-    // Display nodes of both lists
-    firstList.displayNodes();
-    secondList.displayNodes();
-
-    // Compute value sum of both lists
-    int sumOfLists = computeSumOfLists(firstList, secondList);
-    System.out.println("Sum of Lists: " + sumOfLists);
-
-    // Insert each digit from the computed sum of lists in new linked list in reverse order
-    SingleLinkedList sumList = convertValueToList(sumOfLists);
-
-    sumList.displayNodes();
+    detailsBeforeOperation(firstList, secondList);
+    getSumList(firstList, secondList).displayNodes();
   }
 
+  private static void detailsBeforeOperation(SingleLinkedList firstList, SingleLinkedList secondList) {
+    firstList.displayNodes();
+    secondList.displayNodes();
+  }
+
+  private static SingleLinkedList getSumList(SingleLinkedList firstList, SingleLinkedList secondList) {
+    int sumOfLists = computeSumOfLists(firstList, secondList);
+    return convertValueToList(sumOfLists);
+  }
+
+
   private static int computeSumOfLists(SingleLinkedList firstList, SingleLinkedList secondList) {
-    System.out.println("First value: " + convertListToValue(firstList));
-    System.out.println("Second value: " + convertListToValue(secondList));
     return convertListToValue(firstList)
             + convertListToValue(secondList);
   }
 
   private static int convertListToValue(SingleLinkedList listValue) {
     StringBuilder value = buildValue(listValue);
-    int reversedValue = Integer.parseInt(value.reverse().toString());
-    System.out.println("Value Sum String Representation: " + reversedValue);
-    return reversedValue;
+    return Integer.parseInt(value.reverse().toString());
   }
 
   private static StringBuilder buildValue(SingleLinkedList listValue) {
     StringBuilder valueBuilder = new StringBuilder(listValue.size);
-    appendEachNodeValueAsDigit(listValue, valueBuilder);
+    appendEachNodeValue(listValue, valueBuilder);
     return valueBuilder;
   }
 
-  private static void appendEachNodeValueAsDigit(SingleLinkedList listValue, StringBuilder value) {
+  private static void appendEachNodeValue(SingleLinkedList listValue, StringBuilder value) {
     Node currentNode = listValue.head;
     for (int location = 0; location < listValue.size; location++) {
       value.append(currentNode.value);
@@ -51,7 +47,6 @@ public final class SumLists {
   }
 
   private static SingleLinkedList convertValueToList(int value) {
-    // Create new list to contain digits of the sum of lists in reverse order
     SingleLinkedList sumList = new SingleLinkedList();
     insertDigitsToList(value, sumList);
     return sumList;
