a9f635321ed2b4d2e6fa3e1426af476b4ab6e172
apexDev37 <mwangi.em37@gmail.com>
2023-03-02 14:21:15 +0300

    refactor: Modularize intersection iml
    Modularize intersection implementation by extracting functions into smaller logic units.
    Remove unnecessary comments due to expressive naming.
    Adhere to clean code principles for functions.

--- interviewQuestions/linkedList/intersection/Intersection.java
+++ interviewQuestions/linkedList/intersection/Intersection.java
@@ -3,6 +3,7 @@ package com.apexdev.interviewQuestions.linkedList.intersection;
 import com.apexdev.algorithms.java.singlyLinkedList.SingleLinkedList;
 import com.apexdev.algorithms.java.singlyLinkedList.base.Node;
 
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public final class Intersection {
@@ -10,34 +11,9 @@ public final class Intersection {
   private static Node shorter;
 
   public static void execute(SingleLinkedList firstList, SingleLinkedList secondList) {
-    System.out.println("Intersection implementation----------------");
     initializeFields(firstList, secondList);
-
-    System.out.println(longer.value);
-    System.out.println(shorter.value);
-
-    int difference = firstList.size - secondList.size;
-    Node longerStartingNode = getNodeAfterExtraNodes(difference);
-    System.out.printf("Starting node: %d%n", longerStartingNode.value);
-
-
-    // Find the kth Node
-    longer = longerStartingNode;
-    while (longer.next != null) {
-      longer = longer.next;
-      shorter = shorter.next;
-      if (Objects.equals(longer, shorter)) {
-        System.out.println("Found Kth Node");
-        System.out.println("Kth Node: " + longer.value);
-      }
-    }
-  }
-
-  private static Node getNodeAfterExtraNodes(int difference) {
-    Node currentNode = longer;
-    for (int location = 0; location < difference; location++)
-      currentNode = currentNode.next;
-    return currentNode;
+    Node kthNode = findKthNode(firstList, secondList);
+    outputDetails(kthNode);
   }
 
   private static void initializeFields(SingleLinkedList firstList, SingleLinkedList secondList) {
@@ -49,5 +25,41 @@ public final class Intersection {
   private static void assignLongerShorter(SingleLinkedList longerList, SingleLinkedList shorterList) {
     longer = longerList.head;
     shorter = shorterList.head;
+  }
+
+  private static Node findKthNode(SingleLinkedList firstList, SingleLinkedList secondList) {
+    ignoreExtraNodesInLongerList(firstList, secondList);
+    return getKthNode();
+  }
+
+  private static void ignoreExtraNodesInLongerList(SingleLinkedList firstList, SingleLinkedList secondList) {
+    int extraNodes = Math.abs(firstList.size - secondList.size);
+    longer = getNodeAfter(extraNodes);
+  }
+
+  private static Node getNodeAfter(int extraNodes) {
+    Node currentNode = longer;
+    for (int location = 0; location < extraNodes; location++)
+      currentNode = currentNode.next;
+    return currentNode;
+  }
+
+  private static Node getKthNode() {
+    while (longer.next != null) {
+      updateToNextNodes();
+      if (Objects.equals(longer, shorter))
+        return longer;
+    }
+    throw new NoSuchElementException("Intersection node (kth)  does not exist");
+  }
+
+  private static void updateToNextNodes() {
+    longer = longer.next;
+    shorter = shorter.next;
+  }
+
+  private static void outputDetails(Node kthNode) {
+    System.out.printf("[Kth node] -> reference: %s, value: %d%n",
+            kthNode, kthNode.value);
   }
 }
