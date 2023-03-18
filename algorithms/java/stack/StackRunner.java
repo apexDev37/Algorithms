eb9ddb369ce580109c22875399c565fdf0c8c376
apexDev37 <mwangi.em37@gmail.com>
2023-03-18 14:23:36 +0300

    chore: Update driver class
    Update driver class by including client code for the linked stack implementation. Provide clear and intuitive examples for interested parties.

--- algorithms/java/stack/StackRunner.java
+++ algorithms/java/stack/StackRunner.java
@@ -3,15 +3,29 @@ package com.apexdev.algorithms.java.stack;
 public class StackRunner {
 
   public static void main(String[] args) {
-    ArrayStack<String> gangOfFour = new ArrayStack<>(String.class, 4);
+    // Implementation of stack with a fixed capacity
+    Stack<String> gangOfFour = new ArrayStack<>(String.class, 4);
     gangOfFour.push("Erich");
     gangOfFour.push("Richard");
     gangOfFour.push("Ralph");
 
     gangOfFour.push("Eugene");
-    gangOfFour.peek();
-    gangOfFour.pop();
+    System.out.println("Oops! Pop element: " + gangOfFour.pop());
 
     gangOfFour.push("John");
+    System.out.println(gangOfFour);
+
+    // Implementation of stack without fixed capacity
+    Stack<Integer> leapYears = new LinkedStack<>();
+    leapYears.push(2012);
+    leapYears.push(2016);
+    leapYears.push(2020);
+    leapYears.push(2024);
+    leapYears.push(2028);
+    leapYears.push(2032);
+
+    System.out.println(leapYears);
+    System.out.println("Popped element: " + leapYears.pop());
+    System.out.println("Peek top element: " + leapYears.peek());
   }
 }
