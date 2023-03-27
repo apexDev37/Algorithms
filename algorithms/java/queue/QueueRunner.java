87ac89855fdee94f97b7fef0e7a79651755af85e
apexDev37 <mwangi.em37@gmail.com>
2023-03-27 12:46:48 +0300

    chore: Construct circular queue
    Construct circular queue with QueueFactory.
    Provide intuitive examples for interested parties.
    Test drive logic and partially demonstrate class behavior.

--- algorithms/java/queue/QueueRunner.java
+++ algorithms/java/queue/QueueRunner.java
@@ -1,5 +1,8 @@
 package com.apexdev.algorithms.java.queue;
 
+import com.apexdev.algorithms.java.queue.base.Queue;
+import com.apexdev.algorithms.java.queue.implementation.QueueFactory;
+
 public class QueueRunner {
 
   public static void main(String[] args) {
@@ -14,5 +17,19 @@ public class QueueRunner {
     primitives.enqueue("double");
 
     System.out.println(primitives);
+
+    // Instantiate memory efficient queue with a fixed capacity
+    Queue<String> faang = QueueFactory.newReusable(5);
+    faang.enqueue("Facebook");
+    faang.enqueue("Apple");
+    faang.enqueue("Amazon");
+    faang.enqueue("Netflix");
+    faang.enqueue("Google");
+
+    faang.dequeue();
+    faang.enqueue("Meta");
+
+    faang.dequeue();
+    System.out.println(faang);
   }
 }
