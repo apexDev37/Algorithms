181ea3d261570211d8ba57905726c25ed1f809a9
apexDev37 <mwangi.em37@gmail.com>
2023-04-04 13:04:41 +0300

    chore: Override tostring
    Override toString method for debugging purposes and to provide a semantic string representation of class instances.

--- algorithms/java/queue/implementation/LinkedQueue.java
+++ algorithms/java/queue/implementation/LinkedQueue.java
@@ -2,6 +2,8 @@ package com.apexdev.algorithms.java.queue.implementation;
 
 import com.apexdev.algorithms.java.queue.base.LinearDynamicQueue;
 
+import java.text.MessageFormat;
+
 public class LinkedQueue<E> extends LinearDynamicQueue<E> {
 
   @Override
@@ -37,5 +39,26 @@ public class LinkedQueue<E> extends LinearDynamicQueue<E> {
   @Override
   public boolean isFull() {
     return false;
+  }
+
+  @Override
+  public String toString() {
+    return MessageFormat.format(
+            "Queue '{'\n\telements={0}\n\tsize={1}\n'}'",
+            displayElements(),
+            size);
+  }
+
+  private String displayElements() {
+    StringBuilder builder = new StringBuilder("[");
+
+    Node<E> element = this.front;
+    for (int location = 0; location < this.size - 1; location++) {
+      builder.append(element.value).append(", ");
+      element = element.next;
+    }
+
+    builder.append(element.value).append("]");
+    return builder.toString();
   }
 }
