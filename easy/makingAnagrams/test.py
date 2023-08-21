dff72b64a63520706801f80e2740c05adc234fb5
apexdev <mwangi.em37@gmail.com>
2023-08-21 23:00:30 +0300

    test: Add base testcases
    Add two TestCase classes: One for unit logic and integration logic.

--- easy/makingAnagrams/test.py
+++ easy/makingAnagrams/test.py
@@ -1,10 +1,7 @@
 import unittest
+from unittest import TestCase
 
-# from main import (
-#     # function_example_one,
-#     # function_example_two,
-#     # function_example_three
-# )
+from main import is_anagram, making_anagrams
 
 
 # ---------------------------- Tests ---------------------------- #
@@ -16,8 +13,42 @@ Script: main.py
 """
 
 
-# See example sample tests in
-# .
-# |-- easy
-#   |-- question
-#       |-- test.py
+class FunctionalTestCase(TestCase):
+  """
+  Test case to test the functional and unit logic.
+  """
+  
+  def test_should_return_true_if_two_given_strings_are_anagrams(self) -> None:
+    # Given
+    s1 = 'debug'
+    s2 = 'dgebu'
+    expected = True
+    
+    # When
+    actual = is_anagram(s1, s2)
+    
+    # Then
+    self.assertEqual(actual, expected)
+
+class IntegrationTestCase(TestCase):
+  """
+  Test case to test the integration and behavior of HackerRank entry-point. 
+  """
+
+  def setUp(self) -> None:
+    self.under_test = making_anagrams
+
+  def test_should_return_zero_deletions_when_two_strings_are_anagrams(self) -> None:
+    # Given
+    s1 = 'silent'
+    s2 = 'listen'
+    
+    # When
+    actual = self.under_test(s1, s2)
+        
+    # Then
+    self.assertEqual(actual, 0)
+  
+  
+if __name__ == '__main__':
+  unittest.main()
