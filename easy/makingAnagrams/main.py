b7d754f1b90cae1fddf6196a0ad54f3dd7e92e41
apexdev <mwangi.em37@gmail.com>
2023-08-25 23:23:55 +0300

    feat: Add truncate logic
    Add logic to truncate the uncommon chars.
    (chore) Express and achieve algorithm requirements.
    (chore) Ensure all tests are green.

--- easy/makingAnagrams/main.py
+++ easy/makingAnagrams/main.py
@@ -24,6 +24,13 @@ def get_intersection(s1: str, s2: str) -> bool:
   common_chars: set = set(s1) & set(s2)
   return ''.join(common_chars)
 
+def truncate_symmetric_diff(s1: str, s2: str) -> dict[str, str]:
+  uncommon_set = set(s1) ^ set(s2)
+  uncommon_chars = ''.join(uncommon_set)
+  truncated_s1 = s1.rstrip(uncommon_chars)
+  truncated_s2 = s2.rstrip(uncommon_chars)
+  return {'s1': truncated_s1, 's2': truncated_s2}
+
 def main() -> None:
   s1, s2 = 'anagram', 'instagram'
   print('Number of deletions to make anagram: ', making_anagrams(s1, s2))
