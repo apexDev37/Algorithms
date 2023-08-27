e970db676f5584d2f51e4df39b1cc6d66a3a5d0e
apexdev <mwangi.em37@gmail.com>
2023-08-27 23:43:21 +0300

    refactor: Extract function
    Extract function to modularize and break down logic.
    Remove redundant helper comment to get started.

--- easy/makingAnagrams/main.py
+++ easy/makingAnagrams/main.py
@@ -6,9 +6,6 @@
     Implementation: apexDev37
 """
 
-# This is main entry point function to complete
-# on the HackerRank platform. 
-# Rename as per specified
 
 def making_anagrams(s1: str, s2: str) -> int:
   if is_anagram(s1, s2):
@@ -27,9 +24,12 @@ def get_intersection(s1: str, s2: str) -> bool:
 def truncate_symmetric_diff(s1: str, s2: str) -> dict[str, str]:
   uncommon_set = set(s1) ^ set(s2)
   uncommon_chars = ''.join(uncommon_set)
-  truncated_s1 = s1.rstrip(uncommon_chars)
-  truncated_s2 = s2.rstrip(uncommon_chars)
-  return {'s1': truncated_s1, 's2': truncated_s2}
+  return {'s1': truncate(uncommon_chars, s1), 
+          's2': truncate(uncommon_chars, s2)}
+
+def truncate(chars: str, given: str) -> str:
+  result = given.rstrip(chars)
+  return result
 
 def main() -> None:
   s1, s2 = 'anagram', 'instagram'
