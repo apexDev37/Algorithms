a11f7ac28903eca76ba253b16d34c5c7e452dead
apexdev <mwangi.em37@gmail.com>
2023-10-02 23:23:27 +0300

    test: Add initial testcase
    Add the following test case classes:
    - Unit
    - Integration

--- easy/gameOfThrones/test.py
+++ easy/gameOfThrones/test.py
@@ -1,23 +1,42 @@
 import unittest
 
-# from main import (
-#     # function_example_one,
-#     # function_example_two,
-#     # function_example_three
-# )
+from main import (
+  count_char_frequency,
+)
 
 
 # ---------------------------- Tests ---------------------------- #
 
 
 """
-Tests for problem: 'Problem name'
+Tests for problem: Game of Thrones - I
 Script: main.py
 """
 
 
-# See example sample tests in
-# .
-# |-- easy
-#   |-- question
-#       |-- test.py
+class TestCaseUnit(unittest.TestCase):
+  """"
+  Tests to cover functional and unit logic.
+  """
+  
+  def test_should_map_char_to_frequency_for_given_string(self) -> None:
+    # Given
+    s = 'baaxaab'
+    expected = {'b': 2, 'a': 4, 'x': 1}
+
+    # When
+    actual = count_char_frequency(s)
+    
+    # Then
+    self.assertDictEqual(actual, expected)
+
+  
+class TestCaseIntegration(unittest.TestCase):
+  """
+  Tests to cover the integration and behavior of HackerRank entry-point.
+  """
+  pass
+
+
+if __name__ == '__main__':
+  unittest.main()
