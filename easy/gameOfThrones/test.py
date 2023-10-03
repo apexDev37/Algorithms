31ddb06fd6051494abcbf90b0b4578c6b821c0b4
apexdev <mwangi.em37@gmail.com>
2023-10-03 22:42:21 +0300

    test: Update problem tests
    Update tests to cover the required logic for the problem.
    Use TDD design technique to create solution.

--- easy/gameOfThrones/test.py
+++ easy/gameOfThrones/test.py
@@ -2,6 +2,8 @@ import unittest
 
 from main import (
   count_char_frequency,
+  game_of_thrones,
+  is_palindrome,
 )
 
 
@@ -16,7 +18,7 @@ Script: main.py
 
 class TestCaseUnit(unittest.TestCase):
   """"
-  Tests to cover functional and unit logic.
+  Tests to cover unit and concrete implementation.
   """
   
   def test_should_map_char_to_frequency_for_given_string(self) -> None:
@@ -30,12 +32,41 @@ class TestCaseUnit(unittest.TestCase):
     # Then
     self.assertDictEqual(actual, expected)
 
-  
+  def test_should_only_have_one_char_with_odd_frequency_in_map(self) -> None:
+    # Given
+    char_freq_map = {'b': 2, 'a': 4, 'x': 1}
+    expected = True
+    
+    # When
+    actual = is_palindrome(char_freq_map)
+    
+    # Then
+    self.assertIsInstance(actual, bool)
+    self.assertEqual(actual, expected)
+    
 class TestCaseIntegration(unittest.TestCase):
   """
   Tests to cover the integration and behavior of HackerRank entry-point.
+
+  Expected HackerRank return value: 'YES' | 'NO'
   """
-  pass
+
+  def setUp(self) -> None:
+    super().setUp()
+    self.entry_point = game_of_thrones
+  
+  def test_should_return_binary_answer_if_string_can_be_palindrome(self) -> None:
+    # Given
+    s = 'cdefghmnopqrstuvw'
+    expected = 'NO'
+    
+    # When
+    actual = self.entry_point(s)
+    
+    # Then
+    self.assertIsInstance(actual, str)
+    self.assertIn(actual, ['YES', 'NO'])
+    self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
